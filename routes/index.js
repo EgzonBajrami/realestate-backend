600b1b5a96d868f2a2ec4e8329ccea2a87ec82ed
EgzonBajrami <39570496+EgzonBajrami@users.noreply.github.com>
2022-11-10 01:02:30 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,7 @@ const districtController = require('../controller/district.controller')
 
 /* GET home page. */
 router.get("/", (req, res) => {
-res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+
  });
 router.post('/register', async(req,res)=>{
   try{
