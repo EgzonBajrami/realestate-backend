24323bc61b59ed82816955c9c9ddfd2ddbe9e2d4
EgzonBajrami <39570496+EgzonBajrami@users.noreply.github.com>
2022-11-10 01:02:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,8 @@ const propertyRoutes = require('./routes/property.routes')
 
 var app = express();
 const cors = require('cors');
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
